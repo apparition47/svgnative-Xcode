c4ef4ddaad6ec33e7294788b0e11f3dc1819fffd
Aaron Lee <aaron@onefatgiraffe.com>
2025-04-02 16:20:55 +0900

    use expat

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
             dependencies: ["boost", "cpp-base64"],
             path: "svg-native-viewer",
             exclude: ["svgnative/src/win",
-                      "svgnative/src/xml/ExpatXMLParser.cpp",
+                      "svgnative/src/xml/RapidXMLParser.cpp",
                       "svgnative/src/xml/LibXMLParser.cpp",
                       "svgnative/src/ports/cairo",
                       "svgnative/src/ports/d2d",
